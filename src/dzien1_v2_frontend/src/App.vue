48e5448c94fd4a917d68fd3bb33b25864996df27
jprylo <prylowskikuba@gmail.com>
2024-06-25 22:03:44 +0200

    Added counter

--- src/dzien1_v2_frontend/src/App.vue
+++ src/dzien1_v2_frontend/src/App.vue
@@ -1,28 +1,25 @@
-<script setup>
-import { ref } from 'vue';
-import { dzien1_v2_backend } from 'declarations/dzien1_v2_backend/index';
-let greeting = ref('');
-
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  await dzien1_v2_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
-}
-</script>
-
 <template>
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
+    hej blockchain!
+    {{ licznik }}
+    <button @click="podbij">Podbij</button>
   </main>
 </template>
+
+<script>
+export default{
+  data(){
+    return{
+      licznik: 0
+    }
+  },
+  methods: {
+    podbij(){
+      this.licznik++
+    }
+  }
+}
+</script>
